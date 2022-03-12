c684d37d0fe14cd26485cfe899a1ae083e35ce37
secolema <seanclmn1@gmail.com>
2022-03-12 18:40:57 -0500

    classes

--- basics.py
+++ basics.py
@@ -1,2 +1,10 @@
-list = [x*x for x in range(11)]
-print(list)
+class Fraction:
+	def __init__(self,top,bottom):
+		self.num = top
+		self.den = bottom
+	def __str__(self):
+		return f"{self.num}/{self.den}"
+
+fraction = Fraction(3,5)
+
+print(fraction.__str__())
