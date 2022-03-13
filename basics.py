fe26fa7b7cc714adb3ed4e4386086937f3b8338b
secolema <seanclmn1@gmail.com>
2022-03-13 16:39:05 -0400

    more classes n stuff

--- basics.py
+++ basics.py
@@ -1,10 +1,17 @@
-class Fraction:
-	def __init__(self,top,bottom):
-		self.num = top
-		self.den = bottom
+import random
+
+class Die:
+	def __init__(self,num_sides):
+		self.num_sides = num_sides
+		self.current_value = self.roll()
+	def roll(self):
+		self.current_value = random.randrange(1,self.num_sides+1)
+		return self.current_value
 	def __str__(self):
-		return f"{self.num}/{self.den}"
+		return str(self.current_value)
 
-fraction = Fraction(3,5)
+	def __repr__(self):
+		return "{} sided die: {}".format(self.num_sides,self.current_value)
 
-print(fraction.__str__())
+for i in range(1,6):
+	print(Die(i).__str__())
