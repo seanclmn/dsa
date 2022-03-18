cfc001162e03a56e1ecdd7a1385d61c0f2253a31
secolema <seanclmn1@gmail.com>
2022-03-17 22:50:39 -0400

    O(n) complexity

--- bigO/bigo.py
+++ bigO/bigo.py
@@ -1,7 +1,29 @@
-def sum_of_n(n):
-	sum = 0
-	for i in range(1,n+1):
-		sum += i
-	return sum
+def anagram_solution(s1,s2):
+	still_ok = True
+	if len(s1) != len(s2):	
+		still_ok = False
 
-print(sum_of_n(6))
+	a_list = list(s2)
+	pos_1 = 0
+	
+	while pos_1 < len(s1) and still_ok:
+		pos_2 = 0
+		found = False
+		while pos_2 < len( a_list) and not found:
+			if s1[pos_1] == a_list[pos_2]:
+				found = True
+			else:
+				pos_2 = pos_2 + 1
+		if found:
+			a_list[pos_2] = None
+		else:
+			still_ok = False
+		
+		pos_1 = pos_1 + 1
+
+	return still_ok
+
+print(anagram_solution("apple", "pleap"))  # expected: True
+print(anagram_solution("abcd", "dcba"))  # expected: True
+print(anagram_solution("abcd", "dcda"))  # expected: False
+
